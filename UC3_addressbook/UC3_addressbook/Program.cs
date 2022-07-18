ca8b677f65eb6895da1fc15a8f51005e70fb8d2b
hibaanj <anjumhiba15@gmail.com>
2022-07-19 00:57:58 +0530

    added a method edit() to make changes if neede

---
+++ UC3_addressbook/UC3_addressbook/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC3_addressbook
+{
+
+    class Contactadd
+    {
+
+
+        public static void Main(string[] args)
+        {
+
+            // printing Welcome Message
+            Console.WriteLine("\t\t\t******* WELCOME TO ADDRESS BOOK SYSTEM PROGRAM ******");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+
+            Console.WriteLine("enter your first name - ");
+            String firstname = (Console.ReadLine());
+            Console.WriteLine("enter your last name - ");
+            String lastname = Console.ReadLine();
+            Console.WriteLine("enter your email - ");
+            String email = Console.ReadLine();
+            Console.WriteLine("enter your phone number - ");
+            int phone = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter your zip code - ");
+            int zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter your city - ");
+            String city = Console.ReadLine();
+            Console.WriteLine("enter your state - ");
+            String state = Console.ReadLine();
+            Console.WriteLine("enter your address - ");
+            String address = Console.ReadLine();
+            Addressbook addressbook = new Addressbook();
+
+            addressbook.addcontact(firstname, lastname, email, phone, zip, city, state, address);
+            Console.WriteLine("write 1 if you wzant to display you address book");
+            int ans = Convert.ToInt32(Console.ReadLine());
+            if(ans == 1)
+            {
+                addressbook.display();
+            }
+            else
+            {
+                Console.WriteLine("invalid");
+            }
+            
+
+            //to holde the screen
+            Console.ReadKey();
+        }
+
+
+    }
+}
